72a11072f6ad689e17bca878133b07541bf9c02a
Karl Vince Reyes <98654168+karlwizkrafte@users.noreply.github.com>
2026-05-19 13:13:29 +0800

    comply for the lab's requirements

--- src/main/java/cpe223/karlvince/lab8/DashController.java
+++ src/main/java/cpe223/karlvince/lab8/DashController.java
@@ -3,7 +3,6 @@ package cpe223.karlvince.lab8;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.StackPane;
 
@@ -12,62 +11,55 @@ import java.util.ResourceBundle;
 
 public class DashController implements Initializable {
 
-    private static final String[] CHART_TYPES = {
-        "Bar Chart", "Pie Chart", "Area Chart", "Bubble Chart",
-        "Line Chart", "Scatter Chart", "Stacked Area Chart", "Stacked Bar Chart"
-    };
-
     @FXML private ComboBox<String> datasetCombo;
-    @FXML private ComboBox<String> chartCombo;
-    @FXML private StackPane        chartContainer;
+
+    // one StackPane per tab
+    @FXML private StackPane barChartPane;
+    @FXML private StackPane pieChartPane;
+    @FXML private StackPane areaChartPane;
+    @FXML private StackPane bubbleChartPane;
+    @FXML private StackPane lineChartPane;
+    @FXML private StackPane scatterChartPane;
+    @FXML private StackPane stackedAreaChartPane;
+    @FXML private StackPane stackedBarChartPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // dataset selector
         datasetCombo.setItems(FXCollections.observableArrayList("Midterm Results", "Final Results"));
         datasetCombo.setValue("Midterm Results");
 
-        // chart type selector
-        chartCombo.setItems(FXCollections.observableArrayList(CHART_TYPES));
-        chartCombo.setValue("Bar Chart");
+        // refresh all charts whenever the dataset changes
+        datasetCombo.setOnAction(e -> refreshAllCharts());
 
-        // refresh chart on change
-        datasetCombo.setOnAction(e -> refreshChart());
-        chartCombo.setOnAction(e -> refreshChart());
-
-        // initial render
-        refreshChart();
+        // initial render for all tabs
+        refreshAllCharts();
     }
 
-    private void refreshChart() {
-        boolean midterm   = StudentData.isMidterm(datasetCombo.getValue());
-        String  chartType = chartCombo.getValue();
+    private void refreshAllCharts() {
+        boolean midterm = StudentData.isMidterm(datasetCombo.getValue());
 
-        Node chart = buildChart(chartType, midterm);
+        barChartPane.getChildren().setAll(
+            ChartImpl.createBarChart(StudentData.getScores(midterm)));
 
-        chartContainer.getChildren().setAll(chart);
-    }
+        pieChartPane.getChildren().setAll(
+            ChartImpl.createPieChart(StudentData.getGradeDist(midterm)));
 
-    private Node buildChart(String chartType, boolean midterm) {
-        switch (chartType) {
-            case "Bar Chart":
-                return ChartImpl.createBarChart(StudentData.getScores(midterm));
-            case "Pie Chart":
-                return ChartImpl.createPieChart(StudentData.getGradeDist(midterm));
-            case "Area Chart":
-                return ChartImpl.createAreaChart(StudentData.getAverages(midterm));
-            case "Bubble Chart":
-                return ChartImpl.createBubbleChart(StudentData.getStudyData(midterm));
-            case "Line Chart":
-                return ChartImpl.createLineChart(StudentData.getScores(midterm));
-            case "Scatter Chart":
-                return ChartImpl.createScatterChart(StudentData.getStudyData(midterm));
-            case "Stacked Area Chart":
-                return ChartImpl.createStackedAreaChart(StudentData.getScores(midterm));
-            case "Stacked Bar Chart":
-                return ChartImpl.createStackedBarChart(StudentData.getScores(midterm));
-            default:
-                return ChartImpl.createBarChart(StudentData.getScores(midterm));
-        }
+        areaChartPane.getChildren().setAll(
+            ChartImpl.createAreaChart(StudentData.getAverages(midterm)));
+
+        bubbleChartPane.getChildren().setAll(
+            ChartImpl.createBubbleChart(StudentData.getStudyData(midterm)));
+
+        lineChartPane.getChildren().setAll(
+            ChartImpl.createLineChart(StudentData.getScores(midterm)));
+
+        scatterChartPane.getChildren().setAll(
+            ChartImpl.createScatterChart(StudentData.getStudyData(midterm)));
+
+        stackedAreaChartPane.getChildren().setAll(
+            ChartImpl.createStackedAreaChart(StudentData.getScores(midterm)));
+
+        stackedBarChartPane.getChildren().setAll(
+            ChartImpl.createStackedBarChart(StudentData.getScores(midterm)));
     }
 }
